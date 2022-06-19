caaaa11c15d7d81e23e8021ec519dcae8172095b
Sandro Matos <sandro.desenvolvedor@gmail.com>
2022-06-19 16:08:35 -0300

    GridContainer component

---
+++ src/components/GridContainer/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./styles.css";
+
+export type GridContainerProps = {
+  children?: React.ReactNode;
+};
+
+const GridContainer = ({ children }: GridContainerProps) => {
+  return (
+    //
+
+    <div className="ContainerMain"></div>
+  );
+};
+
+export default GridContainer;
