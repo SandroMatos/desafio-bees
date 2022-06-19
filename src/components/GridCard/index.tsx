1dd47e9a887159b88c14e0c74c4b1e20861b70cd
Sandro Matos <sandro.desenvolvedor@gmail.com>
2022-06-19 18:04:44 -0300

    Handle Delete, Handle Input functionalities

--- src/components/GridCard/index.tsx
+++ src/components/GridCard/index.tsx
@@ -3,10 +3,13 @@ import { FaTrashAlt as TrashIcon } from "react-icons/fa";
 import { GoLocation, GoGraph } from "react-icons/go";
 import { BsTelephone as TelephoneIcon } from "react-icons/bs";
 import { GrAddCircle as AddCircleIcon } from "react-icons/gr";
+import { AiOutlineCheckCircle as CheckCircleIcon } from "react-icons/ai";
 import GridCardOption from "../GridCardOptions";
-
 import "./styles.css";
 import { GridCardProps } from "../../constants/components/GridCard/interfaces";
+import { CardsDataTypes } from "../../constants/contexts/Cards/interfaces";
+import { useContext, useState } from "react";
+import CardContext from "../../contexts/CardContext/Context";
 
 const GridCard = ({
   id,
@@ -17,10 +20,37 @@ const GridCard = ({
   opt2,
   opt3,
 }: GridCardProps) => {
+  const [flagInput, setFlagInput] = useState<boolean>(false);
+  const [option, setOption] = useState<string>("Add more");
+  const { cardsData, setCardsData } = useContext(CardContext);
+
+  const handleInput = () => {
+    setOption("");
+    setFlagInput(true);
+  };
+
+  const handleOption = (e: string) => {
+    if (e === "") {
+      setOption("Add more");
+    }
+
+    setFlagInput(false);
+  };
+
+  const handleDeleteCard = () => {
+    let arr: Array<CardsDataTypes> = cardsData.filter((card) => {
+      if (card.id === id) {
+        return;
+      }
+      return card;
+    });
+
+    setCardsData(arr);
+  };
   return (
     //
     <div className="CardContainer">
-      <TrashIcon />
+      <TrashIcon onClick={handleDeleteCard} />
       <div className="ContainerContent">
         <h1 className="CardTitle">{title}</h1>
         <p className="CardAddress">{endereco1}</p>
@@ -39,8 +69,33 @@ const GridCard = ({
             {opt3}
           </GridCardOption>
           <GridCardOption>
-            <AddCircleIcon />
-            Option 4
+            <span
+              className="SpanOption"
+              style={{ width: "100%", height: "100%", cursor: "pointer" }}
+              onClick={() => handleInput()}
+              data-testid="Opt4"
+            >
+              {flagInput ? (
+                <>
+                  <CheckCircleIcon />
+                  <input
+                    type="text"
+                    autoFocus={true}
+                    onBlur={(e) => handleOption(e.target.value)}
+                    onChange={(e) => setOption(e.target.value)}
+                    value={option}
+                    maxLength={15}
+                    data-testid="InputOpt4"
+                    style={{ maxWidth: "3.5rem" }}
+                  />
+                </>
+              ) : (
+                <>
+                  {option === "Add more" ? <AddCircleIcon /> : ""}
+                  {option}
+                </>
+              )}
+            </span>
           </GridCardOption>
         </div>
       </div>
